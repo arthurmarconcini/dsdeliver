9a2aa8438558b131e4aa9ebb45d7743e65e0c6b7
arthurmarconcini <arthur.m.ramos@hotmail.com>
2021-01-09 15:23:23 -0300

    correctly

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -5,18 +5,20 @@ import Navbar from "./Navbar";
 
 function Routes(){
     return (
+      <>
         <BrowserRouter>
-            <Navbar />
-            <Switch>
-                <Route path="/orders">
-                    <Orders />
-                </Route>
-                <Route path="/">
-                    <Home />
-                </Route>
-            </Switch>
+          <Navbar />
+          <Switch>
+            <Route path="/orders">
+              <Orders />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
         </BrowserRouter>
-    )
+      </>
+    );
 }
 
 export default Routes;
